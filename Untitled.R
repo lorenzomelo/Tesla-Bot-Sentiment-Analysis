5b8c6cd0f0e419c6411d7108d0cdf9e6d32455c4
lorenzomelo <93031165+lorenzomelo@users.noreply.github.com>
2021-11-10 10:02:35 +0100

    Add files via upload

---
+++ Untitled.R
@@ -0,0 +1,26 @@
+# Install the rtweet from CRAN
+install.packages("rtweet")
+install.packages("tidytext")
+
+# Load rtweet package
+library(rtweet)
+library(dplyr)
+library(tidyr)
+library(tidytext)
+
+# Authenticate using your credentials to Twitter’s API by creating an access token. 
+# Steps on getting Twitter access tokens: https://cran.r-project.org/web/packages/rtweet/vignettes/auth.html
+
+# Store api keys
+api_key <- "rxTqQ97Bi4VL4R5KnL6eLTb6v"
+api_secret_key <- "b7Xvpj0rnaZqFsQ2RYfUXrNah4nh7WXB21CPgpnoEU7RJ58Iaw"
+access_token <- "1450915637547708425-zugL8tCvtKISQWKge1LhACqEVk9aoi"
+access_token_secret <- "yy1ZY8VsBtqKfWcuqrMDAT5XGuQkRCNffTAa6jsziPArN"
+
+token <- create_token (app = "Tesla Bot Sentiment Analysis",
+             consumer_key = api_key,
+             consumer_secret = api_secret_key,
+             access_token = access_token,
+             access_secret = access_token_secret)
+
+
