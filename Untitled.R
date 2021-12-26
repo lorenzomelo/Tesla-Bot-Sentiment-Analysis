6f815e03ba90ececceedb14899b1baa5d79bafd9
lorenzomelo <93031165+lorenzomelo@users.noreply.github.com>
2021-12-26 12:07:10 +0100

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,27 +1,16 @@
-# Install the rtweet from CRAN
-install.packages("rtweet")
-install.packages("tidytext")
+library(academictwitteR)
 
-# Load rtweet package
-library(rtweet)
-library(dplyr)
-library(tidyr)
-library(tidytext)
+set_bearer()
 
-# Authenticate using your credentials to Twitter’s API by creating an access token. 
-# Steps on getting Twitter access tokens: https://cran.r-project.org/web/packages/rtweet/vignettes/auth.html
+tweets2 <-
+  get_all_tweets(
+    query = "#Teslabot lang:en",
+    start_tweets = "2020-01-01T00:00:00Z",
+    end_tweets = "2021-11-12T00:00:00Z",
+    file = "blmtweets",
+    data_path = "data2/",
+    n = 100000,
+)
 
-# Store api keys
-api_key <- "rxTqQ97Bi4VL4R5KnL6eLTb6v"
-api_secret_key <- "b7Xvpj0rnaZqFsQ2RYfUXrNah4nh7WXB21CPgpnoEU7RJ58Iaw"
-access_token <- "1450915637547708425-zugL8tCvtKISQWKge1LhACqEVk9aoi"
-access_token_secret <- "yy1ZY8VsBtqKfWcuqrMDAT5XGuQkRCNffTAa6jsziPArN"
-
-token <- create_token (app = "Tesla Bot Sentiment Analysis",
-             consumer_key = api_key,
-             consumer_secret = api_secret_key,
-             access_token = access_token,
-             access_secret = access_token_secret)
-
-sample1 <- search_tweets("#TeslaBot", n = 5, include_rts = FALSE)
-sample1
+df <- as.data.frame(tweets2$text)
+text2 <- write.csv(df, "output.csv")
